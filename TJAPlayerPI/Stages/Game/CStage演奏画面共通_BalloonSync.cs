47ef9fa6c52c94489cf69cb7f64651ca5d83a441
きびなご <kibinago123@outlook.jp>
2026-03-05 17:05:36 +0900

    修正

--- TJAPlayerPI/Stages/Game/CStage演奏画面共通_BalloonSync.cs
+++ TJAPlayerPI/Stages/Game/CStage演奏画面共通_BalloonSync.cs
@@ -13,9 +13,8 @@ namespace TJAPlayerPI
             // ... (既存のコンボチェック等のロジック)
             
             // 1. Breakingアニメーションの開始
-            // 毎回新しく生成し、明示的に開始(t開始)させることで、2回目以降も確実に再生されるようにする。
+            // 毎回新しく生成することで、2回目以降も確実に再生されるようにする。
             actChara.CharaAction_Balloon_Breaking[nPlayer] = new CCounter(0, 10000, 1, TJAPlayerPI.app.Timer);
-            actChara.CharaAction_Balloon_Breaking[nPlayer].t開始();
             
             // 2. Broke/Missアニメーションの開始 (1小節分の長さ)
             
@@ -25,7 +24,6 @@ namespace TJAPlayerPI
                 // 前のアニメーションが残っている可能性を考慮して停止してから新しく生成
                 actChara.CharaAction_Balloon_Broke[nPlayer]?.t停止();
                 actChara.CharaAction_Balloon_Broke[nPlayer] = new CCounter(0, 10000, 1, TJAPlayerPI.app.Timer);
-                actChara.CharaAction_Balloon_Broke[nPlayer].t開始();
                 
                 // 成功時はBreakingを止める
                 actChara.CharaAction_Balloon_Breaking[nPlayer]?.t停止();
@@ -33,10 +31,9 @@ namespace TJAPlayerPI
             
             // 例: ミスした時 (風船チップが通り過ぎた時など)
             // if (ミス判定) {
-            //     actChara.CharaAction_Balloon_Miss[nPlayer]?.t停止();
-            //     actChara.CharaAction_Balloon_Miss[nPlayer] = new CCounter(0, 10000, 1, TJAPlayerPI.app.Timer);
-            //     actChara.CharaAction_Balloon_Miss[nPlayer].t開始();
-            //     actChara.CharaAction_Balloon_Breaking[nPlayer]?.t停止();
+                 actChara.CharaAction_Balloon_Miss[nPlayer]?.t停止();
+                 actChara.CharaAction_Balloon_Miss[nPlayer] = new CCounter(0, 10000, 1, TJAPlayerPI.app.Timer);
+                 actChara.CharaAction_Balloon_Breaking[nPlayer]?.t停止();
             // }
             
             return true;
